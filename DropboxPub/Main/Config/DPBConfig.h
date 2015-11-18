53e9a5f52bf9798537124609177ad9acc6a719e1
Jesus <jesus.a22@hotmail.com>
2015-11-18 11:37:53 +0100

    Create config file

---
+++ DropboxPub/Main/Config/DPBConfig.h
@@ -0,0 +1,18 @@
+//
+//  DPBConfig.h
+//  DropboxPub
+//
+//  Created by Jesus Antonio Gil on 18/11/15.
+//  Copyright © 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//App
+extern NSString * const DPBAppName;
+extern NSString * const DPBAppVersion;
+
+//Dropbox
+extern NSString * const DPBDropbpxAppKey;
+extern NSString * const DPBDropboxAppSecret;
---
+++ DropboxPub/Main/Config/DPBConfig.m
@@ -0,0 +1,18 @@
+//
+//  DPBConfig.m
+//  DropboxPub
+//
+//  Created by Jesus Antonio Gil on 18/11/15.
+//  Copyright © 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import "DPBConfig.h"
+
+
+//App
+NSString * const DPBAppName = @"DropboxPub";
+NSString * const DPBAppVersion = @"1.0";
+
+//Dropbox
+NSString * const DPBDropbpxAppKey = @"14dbydeqhhyxfo2";
+NSString * const DPBDropboxAppSecret = @"alhh4m9k7hwzayq";
